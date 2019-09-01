1d5092fd628957d661b6b8f6d20f5c87db8ab39e
LSJ <fewfree12@naver.com>
2019-09-01 21:05:56 +0900

    프로젝트 생성

---
+++ SummonersWarCopy/Assets/Script/StateMachine/GameState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LSJ
+{
+    public class GameState
+    {
+        public virtual void Begin() { }
+        public virtual void Update() { }
+        public virtual void End() { }
+    }
+}
---
+++ SummonersWarCopy/Assets/Script/Utility/ValuesData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LSJ
+{
+    public static class GameValue
+    {
+        public static int InitInt = -1;
+        public static float InitFloat = -1.0f;
+
+    }
+}
